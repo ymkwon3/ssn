1042f8efed624086bdadd28f1fedde388a6e11da
jin-ho <wlsgh2018@naver.com>
2022-04-09 09:07:36 +0900

    로그인 페이지 추가입니다.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
 import Flex from "../elements/Flex";
+import Input from "../elements/Input";
+import Button from "../elements/Button";
 
 const Login = props => {
   return (
     <>
-      <Flex border="2px solid black" width="400px" height="400px">
-        로그인페이지입니다
+      <Flex fd="column" border="2px solid black" width="400px" height="400px">
+        
+        <Input
+          ph={"아이디를 입력해 주세요!"}
+          label={"ID"}
+        ></Input>
+        <Input
+          
+        ></Input>
+        <Flex>
+          <Button>로그인</Button>
+          <Button>회원가입</Button>
+        </Flex>
       </Flex>
     </>
   );
