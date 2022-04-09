e6818c1dc32406b817db8bd4d4be53551fa9607b
youngmin <ymkwon3@gnu.ac.kr>
2022-04-09 13:14:17 +0900

    merged

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,6 @@ import Input from "../elements/Input"
 
 const Header = props => {
   return (
-<<<<<<< HEAD
-    <Flex width="100%" height="100px" border="1px solid black">
-      <button onClick={() => history.push("/")}>home</button>
-      <button onClick={() => history.push("/userid")}>toUserID</button>
-=======
     <Flex margin="20px auto " padding="10px" jc="space-between" border-bottom="1px solid black">
       <CircleImage></CircleImage>
       <Input
@@ -24,13 +19,14 @@ const Header = props => {
         width="200px" 
         height="50px"
         margin="0 -220px 0 0"
+        onClick={() => history.push("/userid")}
       >방명록</Button>
       <Button 
         width="200px" 
         height="50px"
         margin="0 20px 0 0"
+        onClick={() => history.push("/userid")}
       >로그아웃</Button>
->>>>>>> jinho
     </Flex>
   );
 };
