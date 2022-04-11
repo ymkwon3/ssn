96ddf0c839911be2db8c3257f8bbd1c13b40fee8
youngmin <ymkwon3@gnu.ac.kr>
2022-04-11 14:42:56 +0900

    친구불러오기

--- src/components/FriendsInfo.js
+++ src/components/FriendsInfo.js
@@ -5,9 +5,7 @@ import CircleImage from "../elements/CircleImage";
 import {RESP} from "../shared/response"
 
 const FriendsInfo = props => {
-    const {userinfo} = props;
-    const mockData = RESP.USER.result[userinfo];
-
+    console.log(props)
     return (
         <CircleImage
         />
